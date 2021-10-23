392cec8dfb3870abaa220c9277dbac2e6c115723
Artem <artem.web.developer.it@gmail.com>
2021-10-23 11:10:11 +0000

    Actualizar version

--- config/app.php
+++ config/app.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'version' => '0.2',
+    'version' => '0.2.1',
     'name' => 'Inventaris',
     'env' => env('APP_ENV', 'production'),
     'debug' => (bool) env('APP_DEBUG', false),
