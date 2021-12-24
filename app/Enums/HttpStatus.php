1bfd252e3675d779846caf51b9d6c55000db9213
Artem <artem.web.developer.it@gmail.com>
2021-12-24 23:09:37 +0000

    #10 Creando enum httpStatus

---
+++ app/Enums/HttpStatus.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Enums;
+class HttpStatus
+{
+	public const OK = 200;
+	public const NOCONTENT = 204;
+	public const BADREQUEST = 400;
+	public const FORBIDDEN = 403;
+	public const TEAPOT = 418;
+}
