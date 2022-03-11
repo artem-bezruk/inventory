348a174db20352ba389c7c475b6cfc7d9ab6930f
Artem <artem.web.developer.it@gmail.com>
2022-03-11 11:14:13 +0000

    #14 Modificaciones dashborad

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -1,15 +1,34 @@
 <?php
 namespace App\Http\Controllers\Dashboard;
+use App\Dashboard;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
+    protected $respuesta = [];
     public function __construct()
     {
         $this->middleware('auth');
     }
     public function index ()
     {
-    	return view('dashboard.index');
+        $dashboard = new Dashboard();
+        try {
+            $usuarios = $dashboard->usuarios();
+            $bienes = $dashboard->bienes();
+            $actividades = $dashboard->actividades();
+            $this->respuesta["data"] = (object) [
+                "usuarios" => $usuarios,
+                "bienes" => $bienes,
+                "actividades" => $actividades
+            ];
+        } catch (\Exception $e) {
+            $mensaje = (object) [
+                "tipo" => 'e',
+                "mensaje" => __('Oops! Something went wrong')
+            ];
+            session()->flash('alerta', $mensaje);
+        }
+    	return view('dashboard.index', $this->respuesta);
     }
 }
