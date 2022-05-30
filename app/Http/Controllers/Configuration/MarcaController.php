28de58543193de7a4d62ced46b6d73e1d07e7d6e
Artem <artem.web.developer.it@gmail.com>
2022-05-30 12:02:10 +0000

    #19 Listar marcas

--- app/Http/Controllers/Configuration/MarcaController.php
+++ app/Http/Controllers/Configuration/MarcaController.php
@@ -1,9 +1,13 @@
 <?php
 namespace App\Http\Controllers\Configuration;
+use Validator;
+use App\Marca;
+use App\Enums\HttpStatus;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 class MarcaController extends Controller
 {
+	protected $respuesta = [];
     public function __construct()
     {
         $this->middleware('auth');
@@ -11,5 +15,49 @@ class MarcaController extends Controller
     public function index ()
     {
     	return view('marca.index');
+    }
+    public function marcas (Request $request)
+    {
+    	try {
+            $all = Marca::where('eliminado', 0)->get();
+            $this->respuesta["data"] = [];
+            foreach ($all as $marca) {
+                $this->respuesta["data"][] = (object) [
+                    'id' => $marca->id,
+                    'marca' => __($marca->marca),
+                    'urlMostrar' => route("marca.show", ['locale' => app()->getLocale(), 'marca' => $marca->id]),
+                    'urlEditar' => route("marca.edit", ['locale' => app()->getLocale(), 'marca' => $marca->id]),
+                    'urlEliminar' => route("marca.destroy", ['locale' => app()->getLocale(), 'marca' => $marca->id])
+                ];
+            }
+            if (empty($this->respuesta["data"])) {
+                $httpStatus = HttpStatus::NOCONTENT;
+            }
+            else {
+                $httpStatus = HttpStatus::OK;
+            }
+        } catch (\Exception $e) {
+            $this->respuesta["mensaje"] = $e->getMessage()?? HttpStatus::ERROR();
+            $httpStatus = HttpStatus::ERROR;
+        }
+        return response()->json($this->respuesta, $httpStatus);
+    }
+    public function create()
+    {
+    }
+    public function store(Request $request)
+    {
+    }
+    public function show($id)
+    {
+    }
+    public function edit($id)
+    {
+    }
+    public function update(Request $request, $id)
+    {
+    }
+    public function destroy($id)
+    {
     }
 }
--- routes/configuration/MarcaWebRoutes.php
+++ routes/configuration/MarcaWebRoutes.php
@@ -2,3 +2,10 @@
 static $uri = 'configurations/marks'; 
 static $controller = 'Configuration\MarcaController@'; 
 Route::get($uri, $controller . 'index')->name('marca.index')->middleware('checkRol:marcas,r');
+Route::get($uri . '/create', $controller . 'create')->name('marca.create')->middleware('checkRol:marcas,c-r');
+Route::post($uri, $controller . 'store')->name('marca.store')->middleware('checkRol:marcas,c-r');
+Route::get($uri . '/list', $controller . 'marcas')->name('marca.list')->middleware('checkRol:marcas,r');
+Route::get($uri. '/{marca}/show', $controller . 'show')->name('marca.show')->middleware('checkRol:marcas,r');
+Route::get($uri . '/{marca}/edit', $controller . 'edit')->name('marca.edit')->middleware('checkRol:marcas,r-u');
+Route::put($uri . '/{marca}', $controller . 'update')->name('marca.update')->middleware('checkRol:marcas,r-u');
+Route::delete($uri . '/{marca}', $controller . 'destroy')->name('marca.destroy')->middleware('checkRol:marcas,r-d');
