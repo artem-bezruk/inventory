6a121b44fb6504d754d0bdd97f867d237d1543c1
Artem <artem.web.developer.it@gmail.com>
2022-07-15 22:42:27 +0000

    #23 Listar generos

--- app/Http/Controllers/Configuration/GeneroController.php
+++ app/Http/Controllers/Configuration/GeneroController.php
@@ -1,5 +1,8 @@
 <?php
 namespace App\Http\Controllers\Configuration;
+use Validator;
+use App\Genero;
+use App\Enums\HttpStatus;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 class GeneroController extends Controller
@@ -11,5 +14,49 @@ class GeneroController extends Controller
     public function index ()
     {
     	return view('genero.index');
+    }
+    public function generos (Request $request)
+    {
+        try {
+            $all = Genero::where('eliminado', 0)->get();
+            $this->respuesta["data"] = [];
+            foreach ($all as $genero) {
+                $this->respuesta["data"][] = (object) [
+                    'id' => $genero->id,
+                    'genero' => __($genero->genero),
+                    'urlMostrar' => route("genero.show", ['locale' => app()->getLocale(), 'genero' => $genero->id]),
+                    'urlEditar' => route("genero.edit", ['locale' => app()->getLocale(), 'genero' => $genero->id]),
+                    'urlEliminar' => route("genero.destroy", ['locale' => app()->getLocale(), 'genero' => $genero->id])
+                ];
+            }
+            if (empty($this->respuesta["data"])) {
+                $httpStatus = HttpStatus::NOCONTENT;
+            }
+            else {
+                $httpStatus = HttpStatus::OK;
+            }
+        } catch (\Exception $e) {
+            $this->respuesta["mensaje"] = HttpStatus::ERROR();
+            $httpStatus = HttpStatus::ERROR;
+        }
+        return response()->json($this->respuesta, $httpStatus);
+    }
+    public function create()
+    {
+    }
+    public function store(Request $request)
+    {
+    }
+    public function show($id)
+    {
+    }
+    public function edit($id)
+    {
+    }
+    public function update(Request $request, $id)
+    {
+    }
+    public function destroy($id)
+    {
     }
 }
--- routes/configuration/GeneroWebRoutes.php
+++ routes/configuration/GeneroWebRoutes.php
@@ -2,3 +2,10 @@
 static $uri = 'configurations/genders'; 
 static $controller = 'Configuration\GeneroController@'; 
 Route::get($uri, $controller . 'index')->name('genero.index')->middleware('checkRol:generos,r');
+Route::get($uri . '/create', $controller . 'create')->name('genero.create')->middleware('checkRol:generos,c-r');
+Route::post($uri, $controller . 'store')->name('genero.store')->middleware('checkRol:generos,c-r');
+Route::get($uri . '/list', $controller . 'generos')->name('genero.list')->middleware('checkRol:generos,r');
+Route::get($uri. '/{genero}/show', $controller . 'show')->name('genero.show')->middleware('checkRol:generos,r');
+Route::get($uri . '/{genero}/edit', $controller . 'edit')->name('genero.edit')->middleware('checkRol:generos,r-u');
+Route::put($uri . '/{genero}', $controller . 'update')->name('genero.update')->middleware('checkRol:generos,r-u');
+Route::delete($uri . '/{genero}', $controller . 'destroy')->name('genero.destroy')->middleware('checkRol:generos,r-d');
