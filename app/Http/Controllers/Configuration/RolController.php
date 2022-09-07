a49445330f8aadc9f22bc3a33159ccfeb17c20bb
Artem <artem.web.developer.it@gmail.com>
2022-09-07 21:29:57 +0000

    #26 Listar roles

--- app/Http/Controllers/Configuration/RolController.php
+++ app/Http/Controllers/Configuration/RolController.php
@@ -1,10 +1,13 @@
 <?php
 namespace App\Http\Controllers\Configuration;
+use Validator;
 use App\Rol;
-use App\Http\Controllers\Controller;
+use App\Enums\HttpStatus;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 class RolController extends Controller
 {
+	protected $respuesta = [];
     public function __construct()
     {
         $this->middleware('auth');
@@ -12,5 +15,51 @@ class RolController extends Controller
     public function index ()
     {
     	return view('rol.index');
+	}
+	public function roles (Request $request)
+	{
+		try {
+            $all = Rol::where('eliminado', 0)->get();
+            $this->respuesta["data"] = [];
+            foreach ($all as $rol) {
+                $this->respuesta["data"][] = (object) [
+                    'id' => $rol->id,
+					'rol' => __($rol->rol),
+					'descripcion' => __($rol->descripcion),
+					'prioridad' => $rol->prioridad,
+                    'urlMostrar' => route("rol.show", ['locale' => app()->getLocale(), 'rol' => $rol->id]),
+                    'urlEditar' => route("rol.edit", ['locale' => app()->getLocale(), 'rol' => $rol->id]),
+                    'urlEliminar' => route("rol.destroy", ['locale' => app()->getLocale(), 'rol' => $rol->id])
+                ];
+            }
+            if (empty($this->respuesta["data"])) {
+                $httpStatus = HttpStatus::NOCONTENT;
+            }
+            else {
+                $httpStatus = HttpStatus::OK;
+            }
+        } catch (\Exception $e) {
+            $this->respuesta["mensaje"] = HttpStatus::ERROR();
+            $httpStatus = HttpStatus::ERROR;
+        }
+        return response()->json($this->respuesta, $httpStatus);
+	}
+    public function create()
+    {
+    }
+    public function store(Request $request)
+    {
+    }
+    public function show($id)
+    {
+    }
+    public function edit($id)
+    {
+    }
+    public function update(Request $request, $id)
+    {
+    }
+    public function destroy($id)
+    {
     }
 }
--- routes/configuration/RolWebRoutes.php
+++ routes/configuration/RolWebRoutes.php
@@ -2,3 +2,10 @@
 static $uri = 'configurations/roles'; 
 static $controller = 'Configuration\RolController@'; 
 Route::get($uri, $controller . 'index')->name('rol.index')->middleware('checkRol:roles,r');
+Route::get($uri . '/create', $controller . 'create')->name('rol.create')->middleware('checkRol:roles,c-r');
+Route::post($uri, $controller . 'store')->name('rol.store')->middleware('checkRol:roles,c-r');
+Route::get($uri . '/list', $controller . 'roles')->name('rol.list')->middleware('checkRol:roles,r');
+Route::get($uri. '/{rol}/show', $controller . 'show')->name('rol.show')->middleware('checkRol:roles,r');
+Route::get($uri . '/{rol}/edit', $controller . 'edit')->name('rol.edit')->middleware('checkRol:roles,r-u');
+Route::put($uri . '/{rol}', $controller . 'update')->name('rol.update')->middleware('checkRol:roles,r-u');
+Route::delete($uri . '/{rol}', $controller . 'destroy')->name('rol.destroy')->middleware('checkRol:roles,r-d');
